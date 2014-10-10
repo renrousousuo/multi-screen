6806f29456882edd003a9427dc9e995cbb59a5b6
Akikonata <66875692@qq.com>
2014-10-10 16:33:00 +0800

    removed shadow

--- rich/js/game.js
+++ rich/js/game.js
@@ -328,7 +328,7 @@
 					container = document.getElementById('container');
 					group = new THREE.Object3D();
 					scene.add(group);
-
+					//地球
 					var loader = new THREE.TextureLoader();
 					loader.load('textures/land_ocean_ice_cloud_2048.jpg', function(texture) {
 						var geometry = new THREE.SphereGeometry(200, 20, 20);
@@ -339,39 +339,6 @@
 						var mesh = new THREE.Mesh(geometry, material);
 						group.add(mesh);
 					});
-					// shadow
-					var canvas = document.createElement('canvas');
-					canvas.width = 128;
-					canvas.height = 128;
-
-					var context = canvas.getContext('2d');
-					var gradient = context.createRadialGradient(
-						canvas.width / 2,
-						canvas.height / 2,
-						0,
-						canvas.width / 2,
-						canvas.height / 2,
-						canvas.width / 2
-					);
-					gradient.addColorStop(0.1, 'rgba(210,210,210,1)');
-					gradient.addColorStop(1, 'rgba(255,255,255,1)');
-
-					context.fillStyle = gradient;
-					context.fillRect(0, 0, canvas.width, canvas.height);
-
-					var texture = new THREE.Texture(canvas);
-					texture.needsUpdate = true;
-
-					var geometry = new THREE.PlaneGeometry(300, 300, 3, 3);
-					var material = new THREE.MeshBasicMaterial({
-						map: texture,
-						overdraw: 0.5
-					});
-
-					var mesh = new THREE.Mesh(geometry, material);
-					mesh.position.y = -250;
-					mesh.rotation.x = -Math.PI / 2;
-					group.add(mesh);
 					//飞鸟
 					birds = [];
 					boids = [];
@@ -436,7 +403,10 @@
 		},
 		"game": {
 			"enter": function() {
+				//播放字幕
+				var displayDrama = function() {
 
+				};
 			}
 		}
 	};
