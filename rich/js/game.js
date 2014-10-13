85623b84356149de371b16ed2a40e2b16955b56d
Akikonata <66875692@qq.com>
2014-10-14 00:53:54 +0800

    added map

--- rich/js/game.js
+++ rich/js/game.js
@@ -3,7 +3,7 @@
 	var scene = new THREE.Scene();
 	//创建透视相机
 	var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 200);
-	camera.position.set(0, 30, 0);
+	camera.position.set(0, 50, 100);
 	scene.add(camera);
 	//创建渲染器并添加到画布
 	var renderer = new THREE.WebGLRenderer();
@@ -77,17 +77,78 @@
 		},
 		"game": {
 			"enter": function() {
+				var colors = [
+					0x00a0e9, //休息
+					0x009944, //拳击
+					0xaa89bd, //画符
+					0x7d0000, //使坏
+					0xd1c0a5, //招财猫
+					0xffffff, //普通格子
+				];
+				var map = [{
+					x: 0,
+					y: 0,
+					z: 0,
+					type: 5
+				}, {
+					x: 0,
+					y: 0,
+					z: -1,
+					type: 1
+				}, {
+					x: 1,
+					y: 0,
+					z: -1,
+					type: 5
+				}, {
+					x: 0,
+					y: 0,
+					z: 0,
+					type: 0
+				}, {
+					x: 0,
+					y: 0,
+					z: 0,
+					type: 5
+				}, {
+					x: 0,
+					y: 0,
+					z: 0,
+					type: 2
+				}, {
+					x: 0,
+					y: 0,
+					z: 0,
+					type: 4
+				}, {
+					x: 0,
+					y: 0,
+					z: 0,
+					type: 3
+				}, {
+					x: 0,
+					y: 0,
+					z: 0,
+					type: 5
+				}, {
+					x: 0,
+					y: 0,
+					z: 0,
+					type: 5
+				}]; //设置不同的格子类型和位置
 				//创建36个格子
-				for (var i = 0; i < 36; i++) {
-					var geometry = new THREE.BoxGeometry(10, 10, 10);
-					//随机颜色
-					var color = parseInt(Math.random() * 255) * parseInt(Math.random() * 255) * parseInt(Math.random() * 255);
+				for (var i = 0; i < map.length; i++) {
+					var data = map[i];
+					var geometry = new THREE.BoxGeometry(20, 20, 20);
+					//根据类型设置颜色
+					var color = colors[data.type];
 					var material = new THREE.MeshBasicMaterial({
 						color: color
 					});
 					var cube = new THREE.Mesh(geometry, material);
-					cube.position.x = -100 + 20 * i;
-					cube.position.z = -100;
+					cube.position.x = data.x * 20;
+					cube.position.y = data.y * 20;
+					cube.position.z = data.z * 20;
 					scene.add(cube);
 				}
 				//渲染场景
